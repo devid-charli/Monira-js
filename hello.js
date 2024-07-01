5d9dece5ff18594e0f8b4f53bd6d0306d832b23f
devid-charli <mustakali444888@gmail.com>
2024-07-01 16:48:58 +0600

    If else statement

--- hello.js
+++ hello.js
@@ -1,18 +1,4 @@
 
-var myFather ='string'
-console.log(myFather);
-var myFather ='md'
-console.log(myFather);
 
-var myName = 018273623;
-console.log(typeof myName)
-var numberVariable =9485349
-console.log(numberVariable)
-
-
-var booltype = true ;
-console.log(typeof booltype)
-var moontype = false ;
-console.log(moontype)
   
 
